d9c6944b073d8bd5023d19bc96658cdf9db1e946
iharbeck <iharbeck@91f012ad-703b-4ebb-b4ac-6d446edbf5f8>
2009-11-25 14:22:23 +0000

    wrapper

---
+++ WEB-INF/src/strutter/controller/RequestWrapper.java
@@ -0,0 +1,24 @@
+package strutter.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+import strutter.helper.ActionHelper;
+
+public class RequestWrapper extends HttpServletRequestWrapper 
+{
+	public RequestWrapper(HttpServletRequest request) throws IOException { 
+        super(request); 
+    }
+
+	public boolean isUserInRole(String role) 
+	{
+		return ActionHelper.hasRole(role);
+	}
+
+	public String getRemoteUser() {
+		return ActionHelper.getUsername();
+	} 
+}
