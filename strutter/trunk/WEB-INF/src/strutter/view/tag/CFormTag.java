b272e3d68199de6042f81357eb3daedc9428c33f
iharbeck <iharbeck@91f012ad-703b-4ebb-b4ac-6d446edbf5f8>
2010-04-14 14:01:11 +0000

    performance

--- strutter/trunk/WEB-INF/src/strutter/view/tag/CFormTag.java
+++ strutter/trunk/WEB-INF/src/strutter/view/tag/CFormTag.java
@@ -1,6 +1,12 @@
 package strutter.view.tag;
 
+import javax.servlet.ServletRequest;
+
+import org.htmlparser.Node;
+import org.htmlparser.filters.TagNameFilter;
 import org.htmlparser.tags.FormTag;
+import org.htmlparser.tags.InputTag;
+import org.htmlparser.util.NodeList;
 import org.htmlparser.util.ParserException;
 
 /**
@@ -10,7 +16,14 @@ public class CFormTag extends FormTag
 {
 	private static final long serialVersionUID = 1L;
 
-	public void doSemanticAction() throws ParserException {
+	private String actionfieldname;
+	
+	public CFormTag(String actionfieldname) {
+		 this.actionfieldname = actionfieldname;
+	}
+	
+	public void doSemanticAction() throws ParserException 
+	{
 		super.doSemanticAction();
 		
 		if(this.getAttribute("method") == null)
@@ -18,6 +31,34 @@ public class CFormTag extends FormTag
 			this.setAttribute("method", "POST", '"');
 		}
 		
+		
+		if(actionfieldname != null)
+		{
+			/*NodeList inputtags = formtags.extractAllNodesThatMatch(
+					  new AndFilter (
+					   new TagNameFilter ("input"), new HasAttributeFilter("name", actionfieldname)), true
+			);*/
+
+			// hiddenfield action 
+			InputTag input = new InputTag();
+
+			input.setAttribute("name", actionfieldname, '"');
+			input.setAttribute("type", "hidden", '"');
+				
+
+			// hiddenfield sendertoken
+			InputTag inputToken = new InputTag();
+			
+			inputToken.setAttribute("name", "_TOKEN", '"');
+			inputToken.setAttribute("type", "hidden", '"');
+			inputToken.setAttribute("value", ""+System.currentTimeMillis(), '"');
+			
+			if(getChildren() != null)
+			{
+				getChildren().add(input);
+				getChildren().add(inputToken);
+			}
+		}
 	}
 	/**
 	 * Return the set of end tag names that cause this tag to finish.
