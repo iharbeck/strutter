44990346e69fb3830e83e09d8a13a379f925a967
iharbeck <iharbeck@91f012ad-703b-4ebb-b4ac-6d446edbf5f8>
2010-04-15 13:08:51 +0000

    performance

---
+++ WEB-INF/src/strutter/view/tag/CMetaTag.java
@@ -0,0 +1,55 @@
+package strutter.view.tag;
+
+import javax.servlet.ServletRequest;
+
+import org.htmlparser.Node;
+import org.htmlparser.filters.TagNameFilter;
+import org.htmlparser.nodes.TagNode;
+import org.htmlparser.tags.FormTag;
+import org.htmlparser.tags.InputTag;
+import org.htmlparser.tags.MetaTag;
+import org.htmlparser.util.NodeList;
+import org.htmlparser.util.ParserException;
+
+/**
+ * Represents a FORM tag. IGNORE HTML ERROR
+ */
+public class CMetaTag extends MetaTag 
+{
+	private static final long serialVersionUID = 1L;
+
+	ServletRequest request;
+
+	
+	public CMetaTag(ServletRequest request) {
+		 this.request = request;
+	}
+	
+	public void doSemanticAction() throws ParserException 
+	{
+		super.doSemanticAction();
+
+		// META INFORMATION FOR DECORATOR
+		{
+			//NodeList metatags = nl.extractAllNodesThatMatch(new TagNameFilter ("meta"), true);
+			
+			//for(int i = 0; i < metatags.size(); i++)
+			{
+				//TagNode metatag = (TagNode)metatags.elementAt(i);
+			
+				String name = getAttribute("NAME");
+				if(name != null && name.startsWith("decorator_"))
+					request.setAttribute(name, getAttribute("CONTENT"));
+			}
+		}
+	}
+	
+	/**
+	 * Return the set of end tag names that cause this tag to finish.
+	 * 
+	 * @return The names of following end tags that stop further scanning.
+	 */
+	public String[] getEndTagEnders() {
+		return (new String[] {});
+	}
+}
