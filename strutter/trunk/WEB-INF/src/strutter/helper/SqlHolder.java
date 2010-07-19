aa1df1594770651411f61e23709b70195ae11740
iharbeck <iharbeck@91f012ad-703b-4ebb-b4ac-6d446edbf5f8>
2010-07-19 10:20:12 +0000

    encoding of SQL File

--- strutter/trunk/WEB-INF/src/strutter/helper/SqlHolder.java
+++ strutter/trunk/WEB-INF/src/strutter/helper/SqlHolder.java
@@ -1,7 +1,9 @@
 package strutter.helper;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -50,7 +52,7 @@ public class SqlHolder
 
 		String filepath = clazz.getResource(filename + ".sql").getPath();
 		
-		BufferedReader is = new BufferedReader(new FileReader(filepath));
+		BufferedReader is = new BufferedReader(new InputStreamReader(new FileInputStream(filepath), "UTF8"));
 		
 		StringBuffer buf=null;
 		
