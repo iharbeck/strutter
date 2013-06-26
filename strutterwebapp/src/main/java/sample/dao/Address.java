aa4537b9e5c0d5619a80a8b4747e67bf0422d649
iharbeck <iharbeck@91f012ad-703b-4ebb-b4ac-6d446edbf5f8>
2013-06-26 10:26:31 +0000

    webapp Maven migration

---
+++ strutterwebapp/src/main/java/sample/dao/Address.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2006 Ingo Harbeck.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample.dao;
+
+public class Address
+{
+	String anrede;
+
+	String firstname;
+	String lastname;
+	String street;
+	String city;
+
+	public String getAnrede()
+	{
+		return anrede;
+	}
+
+	public void setAnrede(String anrede)
+	{
+		this.anrede = anrede;
+	}
+
+	public String getCity()
+	{
+		return city;
+	}
+
+	public void setCity(String city)
+	{
+		this.city = city;
+	}
+
+	public String getFirstname()
+	{
+		return firstname;
+	}
+
+	public void setFirstname(String firstname)
+	{
+		this.firstname = firstname;
+	}
+
+	public String getLastname()
+	{
+		return lastname;
+	}
+
+	public void setLastname(String lastname)
+	{
+		this.lastname = lastname;
+	}
+
+	public String getStreet()
+	{
+		return street;
+	}
+
+	public void setStreet(String street)
+	{
+		this.street = street;
+	}
+}
